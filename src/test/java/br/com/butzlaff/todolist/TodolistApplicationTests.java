f392d45240279c345e0fbf8b51bb893c72a331f0
butzlaff <emiliobutz@gmail.com>
2023-10-16 14:38:03 -0300

    todo list completo

---
+++ src/test/java/br/com/butzlaff/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.butzlaff.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
